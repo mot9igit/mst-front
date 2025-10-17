aeb44790642ce2b95d842e8b6ba7910f9bd2f12e
Petropavlovskiy Artiom <artpetropavlovskij@gmail.com>
2025-10-17 13:09:35 +0500

    verified fix

--- src/pages/account/ui/header.vue
+++ src/pages/account/ui/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header__wrapper">
-    <header class="content-wrapper header" id="header">
+    <header class="content-wrapper header" id="header" v-if="this.orgActive.verified == 1">
       <Toast />
       <div class="header__catalog-content">
         <a href="/" class="header__logo-container">
@@ -84,12 +84,15 @@
               }}</span>
             </button>
 
-            <button class="d-button d-button-secondary d-button-rounded header__notification"
-            @click.prevent="showNotificationsModal = true">
+            <button
+              class="d-button d-button-secondary d-button-rounded header__notification"
+              @click.prevent="showNotificationsModal = true"
+            >
               <i class="d-icon-bell header__notification-icon"></i>
-              <div class="status" v-if="notificationsAll.no_read != 0">+{{ notificationsAll.no_read ? notificationsAll.no_read : 0 }}</div>
+              <div class="status" v-if="notificationsAll.no_read != 0">
+                +{{ notificationsAll.no_read ? notificationsAll.no_read : 0 }}
+              </div>
             </button>
-
           </div>
         </div>
       </div>
@@ -145,7 +148,6 @@
             <i class="d-icon-pen2 header__address-edit-icon"></i>
           </span>
         </button>
-
       </div>
     </header>
   </div>
@@ -165,13 +167,12 @@
     </customModal>
   </teleport>
 
-    <customModal
-          v-model="showNotificationsModal"
-          class="notifications__modal"
-        >
-        <notificationsWindow :reload="reloadNotificationsModal" @reloadSuccess="this.reloadNotificationsModal = false"/>
-    </customModal>
-
+  <customModal v-model="showNotificationsModal" class="notifications__modal">
+    <notificationsWindow
+      :reload="reloadNotificationsModal"
+      @reloadSuccess="this.reloadNotificationsModal = false"
+    />
+  </customModal>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex'
@@ -182,7 +183,6 @@ import changeAddressWindow from './changeAddressWindow.vue'
 import requirement from './requirement.vue'
 import notificationsWindow from './notificationsWindow.vue'
 import Toast from 'primevue/toast'
-
 
 export default {
   name: 'ProfileHeader',
@@ -202,8 +202,23 @@ export default {
       data_start: new Date(),
     }
   },
-  emits: ['toggleCatalog', 'toggleVendor', 'toggleCart', 'showRequipments', 'notifications', 'notificationsMobile'],
-  components: { Loader, customModal, changeAddressWindow, SearchField, requirement, Toast, notificationsWindow },
+  emits: [
+    'toggleCatalog',
+    'toggleVendor',
+    'toggleCart',
+    'showRequipments',
+    'notifications',
+    'notificationsMobile',
+  ],
+  components: {
+    Loader,
+    customModal,
+    changeAddressWindow,
+    SearchField,
+    requirement,
+    Toast,
+    notificationsWindow,
+  },
   props: {
     active: {
       type: Boolean,
@@ -219,20 +234,18 @@ export default {
     },
   },
   mounted() {
-
     // if(this.$route.matched[5]?.name == 'WholesaleClientsOffer'){
     //   this.getFromOrgStores().then(() => {
     //   this.getOrgBasketStore()
     //   this.getBasket()
     // })
     // }else{
-      this.getOrgStores().then(() => {
-        this.getOrgBasketOfferStore()
-        this.getBasket()
-        this.getAllNotifications()
-      })
-  //  }
-
+    this.getOrgStores().then(() => {
+      this.getOrgBasketOfferStore()
+      this.getBasket()
+      this.getAllNotifications()
+    })
+    //  }
 
     // mobile header
     let elem = document.querySelector('.main')
@@ -250,9 +263,9 @@ export default {
     // уведомления
     if (this.$route.params.id) {
       this.intervalId = setInterval(() => {
-        this.fetchNotification();
-      }, 40000);
-      this.fetchNotification();
+        this.fetchNotification()
+      }, 40000)
+      this.fetchNotification()
     }
   },
   computed: {
@@ -275,17 +288,16 @@ export default {
       //   }else{
 
       return this.orgStores?.items?.find((el) => el.id == this.basketWarehouse)
-     // }
-
+      // }
     },
   },
   methods: {
     ...mapActions({
       getOrgStores: 'org/getOrgStores',
-    //  getFromOrgStores: 'offer/getFromOrgStores',
+      //  getFromOrgStores: 'offer/getFromOrgStores',
       getOrgBasketStore: 'basket/getOrgBasketStore',
       setOrgBasketStore: 'basket/setOrgBasketStore',
-    //  getOrgBasketOfferStore: 'offer/getOrgBasketOfferStore',
+      //  getOrgBasketOfferStore: 'offer/getOrgBasketOfferStore',
       getBasket: 'basket/getBasket',
       getNewNotification: 'notifications/getNewNotification',
       getAllNotifications: 'notifications/getAllNotifications',
@@ -341,25 +353,30 @@ export default {
       }
     },
 
-    fetchNotification(){
+    fetchNotification() {
       this.getNewNotification({
-        data_start: this.data_start
+        data_start: this.data_start,
       }).then((res) => {
-                for (let i = 0; i < res.data.data?.total; i++) {
-                    setTimeout(() => {
-                        let title = res.data.data.items[i].title;
-                        this.$toast.add({ severity: 'secondary', summary: title, detail: res.data.data.items[i].text, info: res.data.data.items[i], life: 7000 });
-                    }, i * 500);
-                    if(this.showNotificationsModal == true){
-                      this.reloadNotificationsModal = true
-                    }
-                    this.getAllNotifications()
-                }
-            });
+        for (let i = 0; i < res.data.data?.total; i++) {
+          setTimeout(() => {
+            let title = res.data.data.items[i].title
+            this.$toast.add({
+              severity: 'secondary',
+              summary: title,
+              detail: res.data.data.items[i].text,
+              info: res.data.data.items[i],
+              life: 7000,
+            })
+          }, i * 500)
+          if (this.showNotificationsModal == true) {
+            this.reloadNotificationsModal = true
+          }
+          this.getAllNotifications()
+        }
+      })
 
-      this.data_start = new Date();
+      this.data_start = new Date()
     },
-
   },
   watch: {
     basket() {
@@ -376,29 +393,27 @@ export default {
         this.getOrgBasketStore()
       })
     },
-    'showNotificationsModal': function(newVal){
-      if(newVal == false){
-          this.readAllNotifications()
-          this.getAllNotifications()
-        }
-      if(newVal != this.mobileNotificationsShow){
+    showNotificationsModal: function (newVal) {
+      if (newVal == false) {
+        this.readAllNotifications()
+        this.getAllNotifications()
+      }
+      if (newVal != this.mobileNotificationsShow) {
         this.$emit('notificationsMobile', this.showNotificationsModal)
       }
     },
-    notificationsAll: function(newVal){
+    notificationsAll: function (newVal) {
       this.$emit('notifications', newVal.no_read)
     },
-    mobileNotificationsShow: function(newVal){
-      if(this.showNotificationsModal != newVal){
+    mobileNotificationsShow: function (newVal) {
+      if (this.showNotificationsModal != newVal) {
         this.showNotificationsModal = newVal
       }
-
     },
     // '$route.matched[5].name': function(newVal){
     //   this.getOrgBasketStore()
     // }
-
-  }
+  },
 }
 </script>
 <style lang="scss">
@@ -422,49 +437,48 @@ export default {
     }
   }
 }
-.notifications__modal-header{
+.notifications__modal-header {
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin-top: -24px;
 }
-.notifications__modal-header-buttons{
+.notifications__modal-header-buttons {
   display: flex;
   align-items: center;
   justify-content: end;
   gap: 8px;
   max-width: 350px;
-  width:320px;
+  width: 320px;
   margin-right: 42px;
 }
-.notifications__modal-header-buttons button{
+.notifications__modal-header-buttons button {
   min-width: calc(50% - 4px);
   max-width: calc(50% - 4px);
   width: calc(50% - 4px);
   font-size: 16px;
   font-weight: 500;
 }
-.notifications__modal-cansel{
+.notifications__modal-cansel {
   color: #757575;
 }
-.notifications__modal-container{
+.notifications__modal-container {
   width: 100%;
   padding-right: 20px;
 }
-.notifications__modal-container h4{
+.notifications__modal-container h4 {
   font-weight: 600;
   font-size: 14px;
   line-height: 18px;
   color: #757575;
   margin: 20px 0;
-
 }
-.notifications__modal-list{
+.notifications__modal-list {
   display: flex;
   flex-direction: column;
   gap: 16px;
 }
-.notifications__modal-item{
+.notifications__modal-item {
   width: 100%;
   height: auto;
   min-height: 115px;
@@ -474,47 +488,48 @@ export default {
   padding: 16px 16px 16px 19px;
   position: relative;
 }
-.notifications__modal-list-new .notifications__modal-item:after{
+.notifications__modal-list-new .notifications__modal-item:after {
   content: '';
-  position:absolute;
+  position: absolute;
   width: 8px;
   height: 8px;
   border-radius: 8px;
   top: 5px;
   left: 5px;
-  background-color:#f92c0d;
-
+  background-color: #f92c0d;
 }
-.notifications__modal-item-header{
+.notifications__modal-item-header {
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
-.notifications__modal-list .header__notification-icon{
-  width:20px;
-  height:20px;
+.notifications__modal-list .header__notification-icon {
+  width: 20px;
+  height: 20px;
   font-size: 14px;
 }
-.notifications__modal-list-new .header__notification-icon{
-   color:#f92c0d
+.notifications__modal-list-new .header__notification-icon {
+  color: #f92c0d;
 }
-.notifications__modal-list-old .header__notification-icon{
-   color:#757575;
+.notifications__modal-list-old .header__notification-icon {
+  color: #757575;
 }
-.notifications__modal-item-header-left{
+.notifications__modal-item-header-left {
   font-weight: 500;
   font-size: 12px;
   line-height: 123%;
   color: #757575;
 }
-.notifications__modal-item .std-notification__header,.notifications__modal-item h6,.notifications__modal-item .std-notification__span{
-  display:none;
+.notifications__modal-item .std-notification__header,
+.notifications__modal-item h6,
+.notifications__modal-item .std-notification__span {
+  display: none;
 }
-.notifications__modal-item .std-notification__content{
+.notifications__modal-item .std-notification__content {
   margin-top: 8px;
   color: #757575;
 }
-.notifications__modal-container-empty{
-  margin-top:30px;
+.notifications__modal-container-empty {
+  margin-top: 30px;
 }
 </style>
